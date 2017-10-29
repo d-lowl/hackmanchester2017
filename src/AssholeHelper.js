94f9d79dc575d8024c62e35b24b158ad7f4f1c6d
Artur Romanov <arthurslife@ymail.com>
2017-10-29 01:13:26 +0000

    Sound added to helper

--- src/AssholeHelper.js
+++ src/AssholeHelper.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton';
+import Sound from 'react-sound';
 
 class AssholeHelper extends Component {
 
@@ -36,24 +37,35 @@ class AssholeHelper extends Component {
     );
   }
 
+// http://www.freesfx.co.uk/rx2/mp3s/6/18337_1464631212.mp3
+// http://www.freesfx.co.uk/rx2/mp3s/5/16914_1461333028.mp3
+// https://www.myinstants.com/media/sounds/greenscreen-wow.mp3
   render(){
     return(
-      <Card className={"helper " + (this.state.left ? "helper-left" : "")} onMouseEnter={this.hidePopUp}>
-        <CardHeader
-          title="Helper"
-          subtitle="Professional"
-          avatar="./helper.jpg"
+      <div>
+        <Card className={"helper " + (this.state.left ? "helper-left" : "")} onMouseEnter={this.hidePopUp}>
+          <CardHeader
+            title="Helper"
+            subtitle="Professional"
+            avatar="./helper.jpg"
+          />
+          <CardMedia>
+            <img src="./helper.jpg" alt="" />
+          </CardMedia>
+          <CardText>
+            Our professional helper is willing to help you wtih your struggles! Dont hesitate to message him!
+          </CardText>
+          <CardActions>
+            <RaisedButton href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" label="Message instructor" secondary={true}/>
+          </CardActions>
+        </Card>
+
+        <Sound
+        url="https://www.myinstants.com/media/sounds/greenscreen-wow.mp3"
+        playStatus={Sound.status.PLAYING}
+        playFromPosition={100 /* in milliseconds */}
         />
-        <CardMedia>
-          <img src="./helper.jpg" alt="" />
-        </CardMedia>
-        <CardText>
-          Our professional helper is willing to help you wtih your struggles! Dont hesitate to message him!
-        </CardText>
-        <CardActions>
-          <RaisedButton href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" label="Message instructor" secondary={true}/>
-        </CardActions>
-      </Card>
+      </div>
     ); // return
   } // render
 }
